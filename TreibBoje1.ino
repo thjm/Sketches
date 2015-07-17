ea27c9a2ff48ea6a700767ae151a3a7a18e37607
mathes <>
2015-07-17 19:58:34 +0000

    sensor scanning working; sensor readout added and working

--- TreibBoje1.ino
+++ TreibBoje1.ino
@@ -10,6 +10,8 @@
 #define DEBUG
 // scan sensors (to inquire addresses)
 #define SCAN_SENSORS
+// read temperature sensors
+#define READ_SENSORS
 
 #ifdef DEBUG
  // http://arduiniana.org/libraries/streaming/
@@ -51,15 +53,15 @@ DallasTemperature sensors(&oneWire);
 
 // arrays to hold device addresses of known 1-wire temp. sensors
 // sensor #1
-//DeviceAddress gSensor1 = { };
+DeviceAddress gSensor1 = { 0x28, 0xCE, 0x15, 0x46, 0x06, 0x00, 0x00, 0x04 };
 // sensor #2
-//DeviceAddress gSensor2 = {  };
+DeviceAddress gSensor2 = { 0x28, 0x65, 0x5D, 0x47, 0x06, 0x00, 0x00, 0xF8 };
 // sensor #3
-//DeviceAddress gSensor3 = {  };
+DeviceAddress gSensor3 = { 0x28, 0x76, 0xC1, 0xE0, 0x06, 0x00, 0x00, 0x43 };
 // sensor #4
-//DeviceAddress gSensor4 = {  };
+DeviceAddress gSensor4 = { 0x28, 0x7A, 0x31, 0xE0, 0x06, 0x00, 0x00, 0x98 };
 // sensor #5
-//DeviceAddress gSensor5 = {  };
+DeviceAddress gSensor5 = { 0x28, 0x6B, 0x84, 0xDF, 0x06, 0x00, 0x00, 0xBB };
 
 /**
  * Initialization
@@ -95,11 +97,11 @@ void setup()
     if ( sensors.getAddress(addr, nSensor) ) {
       sensors.setResolution(addr, TEMPERATURE_PRECISION);
       Serial << "Device " << (int)nSensor << ": ";
-      //printAddress( addr );
-      for ( byte i=0; i<8; ++i ) {
-        Serial.print(addr[i], HEX);
-        Serial.print(" ");
-      }
+      printAddress( addr );
+//      for ( byte i=0; i<8; ++i ) {
+//        Serial.print(addr[i], HEX);
+//        Serial.print(" ");
+//      }
       Serial.println();
     }
   }
@@ -110,6 +112,34 @@ void setup()
  * Main loop
  */
 void loop() {
+
+#ifdef READ_SENSORS
+  if ( sensors.getDeviceCount() > 0 ) {
+    sensors.requestTemperatures(); // Send the command to get temperatures
+
+#ifdef DEBUG
+    // printout of temperature sensor values in the order they are enumerated at the 1-wire bus
+    // reading will be prefixed via T1: .. T2: ... etc.
+    // alternative printout, only known sensors correctly prefixed
+    if ( sensors.isConnected( gSensor1 ) ) {
+      Serial << "T1: " << sensors.getTempC( gSensor1 ) << " ";
+    }
+    if ( sensors.isConnected( gSensor2 ) ) {
+      Serial << "T2: " << sensors.getTempC( gSensor2 ) << " ";
+    }
+    if ( sensors.isConnected( gSensor3 ) ) {
+      Serial << "T3: " << sensors.getTempC( gSensor3 ) << " ";
+    }
+    if ( sensors.isConnected( gSensor4 ) ) {
+      Serial << "T4: " << sensors.getTempC( gSensor4 ) << " ";
+    }
+    if ( sensors.isConnected( gSensor5 ) ) {
+      Serial << "T5: " << sensors.getTempC( gSensor5 ) << " ";
+    }
+    Serial.println();
+#endif // DEBUG
+  } // if ( getDeviceCount() > 0 )
+#endif // READ_SENSORS
 }
 
 /**
@@ -130,9 +160,11 @@ int availableMemory() {
 void printAddress(DeviceAddress deviceAddress) {
 
   for (uint8_t i = 0; i < 8; i++) {
+    Serial.print("0x");
     // zero pad the address if necessary
     if (deviceAddress[i] < 16) Serial.print("0");
     Serial.print(deviceAddress[i], HEX);
+    Serial.print(" ");
   }
 }
 
